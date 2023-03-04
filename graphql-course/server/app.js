7ea314e6cdae5bcbd6c9546eebf1eaee2865cefe
dc <00danzy@gmail.com>
2023-03-04 02:37:04 +0100

    first commit

--- graphql-course/server/app.js
+++ graphql-course/server/app.js
@@ -1,11 +1,13 @@
 const express     = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 const schema      = require('./schema_graphql/schema');
 const mongoose    = require('mongoose');
+const cors        = require('cors');
 
 
 // Initialize App & Middleware's
 const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
    schema: schema,
@@ -14,9 +16,8 @@ app.use('/graphql', graphqlHTTP({
 
 mongoose.connect("mongodb://127.0.0.1:27017/bookstore")
 mongoose.connection.once('open', () => {
-   console.log("Connected to database")
 
    app.listen(4000, () => {
-      console.log('Now listening for requests on port 4000');
+      console.log('Db Connected 🔌. Now listening for requests on port 4000');
    });
 })
