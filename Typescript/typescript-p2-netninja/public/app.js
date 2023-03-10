2eca648ce9d8fba872ac2357804eb4d44a366329
dc <00danzy@gmail.com>
2023-03-10 23:11:48 +0100

    sample how classes work

--- Typescript/typescript-p2-netninja/public/app.js
+++ Typescript/typescript-p2-netninja/public/app.js
@@ -1,1 +1,13 @@
 "use strict";
+class Test {
+    constructor(t, l) {
+        this.title = t;
+        this.level = l;
+    }
+    format() {
+        return `Test: ${this.title} for the \nLevel: ${this.level}`;
+    }
+}
+let t1 = new Test("CMP 309", 300);
+let t2 = new Test("CMP 325", 300);
+console.log(t1, "\n", t2);
