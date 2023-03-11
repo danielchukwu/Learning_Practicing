239a12f1b18fa5bda2eed2cf46c812d0f1754298
dc <00danzy@gmail.com>
2023-03-11 17:23:51 +0100

    compile

--- Typescript/typescript-p2-netninja/public/app.js
+++ Typescript/typescript-p2-netninja/public/app.js
@@ -1,13 +1,6 @@
 "use strict";
-class Test {
-    constructor(t, l) {
-        this.title = t;
-        this.level = l;
-    }
-    format() {
-        return `Test: ${this.title} for the \nLevel: ${this.level}`;
-    }
-}
-let t1 = new Test("CMP 309", 300);
-let t2 = new Test("CMP 325", 300);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Invoice_js_1 = require("./classes/Invoice.js");
+let t1 = new Invoice_js_1.Test("CMP 309", 300);
+let t2 = new Invoice_js_1.Test("CMP 325", 300);
 console.log(t1, "\n", t2);
