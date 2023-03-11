326e13716024a1980057c80c130621133b9955af
dc <00danzy@gmail.com>
2023-03-11 18:33:36 +0100

    interfaces example

--- Typescript/typescript-p2-netninja/src/app.ts
+++ Typescript/typescript-p2-netninja/src/app.ts
@@ -1,7 +1,32 @@
-import { Invoice, Test } from './classes/Invoice.js';
+const addUID = <T extends {name: string}>(obj: T) => {
+   const id = Math.floor( Math.random() * 100);
+   return obj;
+}
 
-let t1: Test = new Test("CMP 309", 300);
-let t2: Test = new Test("CMP 325", 300);
+const user1 = addUID({name: 'stanley', age: 30});
+// const user2 = addUID({age: 5});
 
-console.log(t1, "\n", t2);
+console.log(user1);
+console.log(user1.name);
 
+// Interface
+interface Car<T> {
+   name: string;
+   model: string;
+   year: T;
+}
+
+const car1: Car<string> = {
+   name: "Mercedes Benz",
+   model: "m5",
+   year: "2019"
+}
+const car2: Car<number> = {
+   name: "Bmw",
+   model: "e11",
+   year: 2015
+}
+
+
+console.log(car1);
+console.log(car2);
