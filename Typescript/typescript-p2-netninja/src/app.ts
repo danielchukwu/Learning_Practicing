aa6c8527b96bd3def5ca205edc54ce58c996a481
dc <00danzy@gmail.com>
2023-03-11 17:24:46 +0100

    imports

--- Typescript/typescript-p2-netninja/src/app.ts
+++ Typescript/typescript-p2-netninja/src/app.ts
@@ -1,19 +1,7 @@
-class Test {
-   private title: string;
-   level: number;
-
-   constructor(t: string, l: number) {
-      this.title = t;
-      this.level = l;
-   }
-
-   private format() {
-      return `Test: ${this.title} for the \nLevel: ${this.level}`;
-   }
-   
-}
+import { Invoice, Test } from './classes/Invoice.js';
 
 let t1: Test = new Test("CMP 309", 300);
 let t2: Test = new Test("CMP 325", 300);
 
 console.log(t1, "\n", t2);
+
